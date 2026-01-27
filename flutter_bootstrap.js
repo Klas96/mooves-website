b722b62b4dcc6307181ca9912a2faf765415cbb9
Klas Holmgren <klas0holmgren@gmail.com>
2026-01-27 20:11:29 +0100

    Update web app with Google authentication fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2362522440"
+    serviceWorkerVersion: "2868989129"
   }
 });
